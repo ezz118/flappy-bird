57cd6c22f0a7134b5595633a3f173af4b4badaa8
Jason Mak <ezz118@connect.hku.hk>
2023-11-15 23:45:50 +0800

    testing of commitment using vsc

--- startgame.cpp
+++ startgame.cpp
@@ -1,6 +1,19 @@
-#include<iostream>
-using namespace std;
 
-int main(){
+int kbhit();
 
+char screen[50][50];
+
+int init();
+
+int main(){//will be renamed startgame in the end
+    init();
+    while (1){
+        if (kbhit()){
+            break;
+        }
+        //usleep(50000);
+    }
+    while (1){
+
+    }
 }
