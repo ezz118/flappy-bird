cf8da0cdd4d510854d0f4b79f3516fbb25c617aa
Ke Wenjun <146153023+poiuy1z@users.noreply.github.com>
2023-11-22 15:57:12 +0800

    Update obstacle.h

--- obstacle.h
+++ obstacle.h
@@ -1,6 +1,6 @@
 #ifndef OBSTACKLE_H
 #define OBSTACKLE_H
-
+#include "bird.h"
 // a class for the parameters of an obstacle
 
 //fix the velocity of the obstacle
@@ -10,18 +10,20 @@ public:
     // get the position of the obstacle in the x direction
     double getx();
     // get the discriminant of whether the bird touches the obstacle
-    bool istouch(Bird bird);
+    bool istouch(Bird B);
     // set the parameters for the obstacle
     void set_position(double x, double hole, double hole_height);
     // update the position of the obstacle in the x direction
     void next();
+    // get the discriminant of whether the bird passed the obstacle
+    bool ispassed(Bird B);
 
 private:
     // position of the obstacle in the x direction
     double x;
     // position of the hole in the y direction
     double hole;
-    // the height of the hole
-    double hole_height;
+    // record whether the bird has passed the obstacle
+    bool pass;
 };
 #endif
