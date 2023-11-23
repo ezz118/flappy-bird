0c803f000a4ed0ef77d78f384e86a42309a1be19
SillyDacian <124056305+SillyDacian@users.noreply.github.com>
2023-11-23 11:46:02 +0800

    Update menu.h

--- menu.h
+++ menu.h
@@ -1,9 +1,13 @@
-//menu.h
+///menu.h
 #ifndef MENU_H
 #define MENU_H
+#include <string>
+using namespace std;
 
-void upd_ranking(int score);
+void upd_ranking(int score, string name);
 
 void showranking();
-void print_caidan();
+
+void print_menu();
+
 #endif
