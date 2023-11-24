155954556307f87c31223d66c972b44a2a509463
Ke Wenjun <146153023+poiuy1z@users.noreply.github.com>
2023-11-24 13:57:47 +0800

    Update obstacle.cpp

--- obstacle.cpp
+++ obstacle.cpp
@@ -7,6 +7,31 @@ double obstacle::getx() {
     return x;
 }
 
+// get the position of the hole
+double obstacle::gethole() {
+    return hole;
+}
+
+// get the pass value
+bool obstacle::getpass() {
+    return pass;
+}
+
+// to set the x coordinate
+void obstacle::setx(double input_x){
+    x = input_x;
+}
+
+// to set the hole position
+void obstacle::sethole(double y){
+    hole = y;
+}
+
+// to set the pass value
+void obstacle::setpass(bool p){
+    pass = p;
+}
+
 // update the position of the obstacle in the x direction
 void obstacle::next() {
     x = x - o_velocity;
@@ -23,12 +48,12 @@ bool obstacle::istouch(Bird B, int screen_N) {
     for (int i=0; i<=1; i++){
         for (int j=-5; j<=6; j++){
             for (int k=0; k<=hole-1; k++){
-                if (((ceil(B.x)+i) == (x+j)) and ((ceil(B.y)-1) == k)){
+                if (((ceil(B.getx())+i) == (x+j)) and ((ceil(B.gety())-1) == k)){
                     return true;
                 }
             }
             for (int k=hole+9; k<=screen_N; k++){
-                if (((ceil(B.x)+i) == (x+j)) and ((ceil(B.y)+1) == k)){
+                if (((ceil(B.getx())+i) == (x+j)) and ((ceil(B.gety())+1) == k)){
                     return true;
                 }
             }
@@ -38,12 +63,12 @@ bool obstacle::istouch(Bird B, int screen_N) {
         if ((i != 0) and (i != 1)){
             for (int j=-5; j<=6; j++){
                 for (int k=0; k<=hole-1; k++){
-                    if (((ceil(B.x)+i) == x+j) and (ceil(B.y) == k)){
+                    if (((ceil(B.getx())+i) == x+j) and (ceil(B.gety()) == k)){
                         return true;
                     }
                 }
                 for (int k=hole+9; k<screen_N; k++){
-                    if (((ceil(B.x)+i) == x+j) and (ceil(B.y) == k)){
+                    if (((ceil(B.getx())+i) == x+j) and (ceil(B.gety()) == k)){
                         return true;
                     }
                 }
@@ -55,6 +80,6 @@ bool obstacle::istouch(Bird B, int screen_N) {
 
 // get the discriminant of whether the bird passed the obstacle
 bool obstacle::ispassed(Bird B){
-    if (ceil(B.x) >= ceil(x)-5)
+    if (ceil(B.getx()) >= ceil(x)-5)
        return true;
 }
