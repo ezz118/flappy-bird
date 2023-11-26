f33eb227dc60e70b706fbf1339248157a9ce1698
Jason Mak <ezz118@connect.hku.hk>
2023-11-26 14:05:18 +0800

    update interactive.cpp

--- interactive.cpp
+++ interactive.cpp
@@ -9,10 +9,12 @@ const int sleep_time=50000;
 #include <termios.h>
 #include <fcntl.h>
 
-bool keyboard_hit(){
-    struct termios oldt, newt;
-    int ch;
-    int oldf;
+typedef struct termios T;
+
+int keyboard_hit(char target){
+    T oldt, newt;
+    int c, oldf;
+
     tcgetattr(STDIN_FILENO, &oldt);
     newt = oldt;
     newt.c_lflag &= ~(ICANON | ECHO);
@@ -20,14 +22,13 @@ bool keyboard_hit(){
     oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
     fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
 
-    ch = getchar();
+    c = getchar();
 
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
     fcntl(STDIN_FILENO, F_SETFL, oldf);
 
-    if(ch != EOF && ch==" "){
-    	ungetc(ch, stdin);
-    	return 1;
+    if(c != EOF && c==target){
+        return 1;
     }
     return 0;
 }
