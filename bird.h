6d9c990e5df407e94e876c108a7b269645acf10f
ruizelai <145953969+ruizelai@users.noreply.github.com>
2023-11-27 22:55:37 +0800

    Update bird.h (adding comments)

--- bird.h
+++ bird.h
@@ -17,28 +17,40 @@ class Bird {
 public:
     // get the position of the bird in the x direction
     double getx();
+
     // get the position of the bird in the y direction
     double gety();
-    // get the velocity of the bird
+
+    // get the velocity of the bird in the y direction
     double getv();
+
     // update the velocity of the bird after every jump
     void jump();
+
     // update the position and the velocity of the bird after every time step
     void next();
-    // set the value of y
+
+    // set the value of y coordinate of the bird
     void sety(double h);
-    // set the value of v
+    // input a constant as the desired coordinate parameter
+
+    // set the value of velocity of the bird
     void setv(double velo);
-    
+    // input a constant as the desired velocity parameter
+
 private:
     // position of the bird in the x direction
     const double x = x_position;
+
     // position of the bird in the y direction
     double y;
+
     // velocity of the bird in the y direction, we set positive direction to be upward
     double v;
+
     // acceleration of the bird in the y direction
     const double a = acceleration;
+
     // velocity of the bird's jump
     const double init_v = jump_velocity;
 
