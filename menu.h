bbd2a7ce558e4ea3daf2c7259bae46db7b3c6ee8
ruizelai <145953969+ruizelai@users.noreply.github.com>
2023-11-27 23:06:18 +0800

    Update menu.h (add comments)

--- menu.h
+++ menu.h
@@ -4,10 +4,20 @@
 #include <string>
 using namespace std;
 
+// a function to update the ranking list;
+// input the score and the name of the player
 void upd_ranking(int score, string name);
+
+// a function to display the ranking list
 void showranking(void);
+
+// a function to print out the menu page
 void print_menu(void);
+
+// a function to pause and enter the next phase
 void wait_to_enter(void);
+
+// a function to end the game
 void game_end(void);
 
 #endif
