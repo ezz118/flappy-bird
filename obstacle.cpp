b1f4f65f15b969fa64a60a2d0b98573e47087fbc
ruizelai <145953969+ruizelai@users.noreply.github.com>
2023-11-27 23:08:46 +0800

    Update obstacle.cpp (delete extra comments)

--- obstacle.cpp
+++ obstacle.cpp
@@ -3,12 +3,10 @@
 #include <cmath>
 #include <cstdio>
 
-// get the position of the obstacle in the x direction
 double obstacle::getx() {
     return x;
 }
 
-// get the position of the hole
 double obstacle::gethole() {
     return hole;
 }
@@ -21,40 +19,34 @@ double obstacle::gethole_upper() {
     return upper;
 }
 
-// get the pass value
 bool obstacle::getpass() {
     return pass;
 }
 
-// to set the x coordinate
 void obstacle::setx(double input_x){
     x = input_x;
 }
 
-// to set the hole position
 void obstacle::sethole(double y){
     hole = y;
     upper = y+11;
     lower = y-3;
 }
 
-// to set the pass value
 void obstacle::setpass(bool p){
     pass = p;
 }
 
-// update the position of the obstacle in the x direction
 void obstacle::next() {
     x = x - o_velocity;
 }
 
-// set the parameters for the obstacle
+
 void obstacle::set_position(double s, double hole_y, double hole_height_y) {
     x = s;
     hole = hole_y;
 }
 
-// get the discriminant of whether the bird touches the obstacle
 bool obstacle::istouch(Bird B, int screen_N) {
     for (int i=0; i<=1; i++){
         for (int j=-5; j<=6; j++){
@@ -97,7 +89,6 @@ bool obstacle::istouch(Bird B, int screen_N) {
 
 }
 
-// get the discriminant of whether the bird passed the obstacle
 bool obstacle::ispassed(Bird B){
     if (ceil(B.getx()) >= ceil(x)-5){
         return true;
