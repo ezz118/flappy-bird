d790c062ca1fd35a15591af4cfa0fc9108b70243
ruizelai <145953969+ruizelai@users.noreply.github.com>
2023-11-27 23:17:21 +0800

    Update startgame.h (delete normal distribution function)

--- startgame.h
+++ startgame.h
@@ -5,33 +5,44 @@
 const int screen_N=35;//size of the screen
 const int screen_M=100;
 
-Bird* init_game(void);//for initializing the game, e.g. new a bird
+//for initializing the game, e.g. new a bird
+Bird* init_game(void);
 
-void print_screen(void);//to print the screen
+// print the screen
+void print_screen(void);
 
-void move_bird(Bird &B);//to move a bird
+//to move a bird
+void move_bird(Bird &B);
 
-void move_obstacles(void);//to move all the obstacles, which are store in STL_list, also delete the obstacles that are out
+//to move all the obstacles, which are store in STL_list, also delete the obstacles that are out
+void move_obstacles(void);
 
-bool check_fail(Bird B, int &score);//to check if the bird hit an obstacle; and check if the bird pass an obstacle
+//to check if the bird hit an obstacle; and check if the bird pass an obstacle
+bool check_fail(Bird B, int &score);
 
-double normal_distribution(int a, int b, double mean, double std);//to generate a random number from normal distribution
+//check if an obstacle will be added now
+bool check_new_ob(int t);
 
-bool check_new_ob(int t);//check if an obstacle will be added now
+//make an ob and push_back into the list
+void add_ob(void);
 
-void add_ob(void);//make an ob and push_back into the list
+//add the bird to screen
+void add_bird_to_screen(Bird B);
 
-void add_bird_to_screen(Bird B);//add the bird to screen
+//add the obstacles to screen
+void add_ob_to_screen(void);
 
-void add_ob_to_screen(void);//add the obstacles to screen
+//to start the game
+int startgame();
 
-int startgame();//to start the game
+//to show the bird falling when it hits the obstacle
+void bird_fall(Bird B);
 
-void bird_fall(Bird B);//to show the bird falling when it hits the obstacle
+//to initialize all the entries in screen to be ' '
+void init_screen(void);
 
-void init_screen(void);//to initialize all the entries in screen to be ' '
-
-bool check_new_ob(int t);//judge whether to add a new obstacle
+//judge whether to add a new obstacle
+bool check_new_ob(int t);
 
 void game_pause(void);//to pause the game
 
