d2901762f467954df41a7501576848909ecd8e40
ruizelai <145953969+ruizelai@users.noreply.github.com>
2023-11-27 23:12:34 +0800

    Update obstacle.h (add comments)

--- obstacle.h
+++ obstacle.h
@@ -15,32 +15,45 @@ class obstacle {
 public:
     // get the position of the obstacle in the x direction
     double getx();
+
     //get the position of the hole
     double gethole();
+
     //to know whether the obstacle has been passed
-    double gethole_lower();
-    double gethole_upper();
+    double gethole_lower();//get the y-position of the lower bound of the hole
+    double gethole_upper();//get the y-position of the upper bound of the hole
     bool getpass();
-    // get the discriminant of whether the bird touches the obstacle
+
+    // get the discriminant of whether the bird touches the obstacle.
+    // input the bird object and the screen size (width), output a boolean 
+    // value to determine whether the bird touches the obstacle
     bool istouch(Bird B, int screen_N);
+
     // set the parameters for the obstacle
     void set_position(double x, double hole, double hole_height);
+
     //set the x coordinate of the obstacle
     void setx(double x);
+
     //set the hole position
     void sethole(double y);
+
     //set the pass value 
     void setpass(bool p);
+
     // update the position of the obstacle in the x direction
     void next();
+
     // get the discriminant of whether the bird passed the obstacle
     bool ispassed(Bird B);
 
 private:
     // position of the obstacle in the x direction
     double x;
+
     // position of the hole in the y direction
     double hole, upper, lower;
+
     // record whether the bird has passed the obstacle
     bool pass;
 };
