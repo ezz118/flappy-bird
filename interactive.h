d40eddd4fdcc70b68124e77dbc393eecc3b096de
ruizelai <145953969+ruizelai@users.noreply.github.com>
2023-11-27 22:58:18 +0800

    Update interactive.h (add comments)

--- interactive.h
+++ interactive.h
@@ -1,8 +1,13 @@
 #ifndef INTERACTIVE_H
 #define INTERACTIVE_H
 
+// a function to clear the screen
 void clear_screen(void);
+
+// a function to print the new screen after a hit on the keyboard
 char keyboard_hit(void);
+
+//create a suspension period
 void wait_to_next(void);
 
 #endif
