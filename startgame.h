9d127a1f0bf9e46bb6f8b6cec1447b76653e6f1e
ezz118 <146748838+ezz118@users.noreply.github.com>
2023-11-29 15:44:48 +0800

    Update startgame.h: add comments

--- startgame.h
+++ startgame.h
@@ -5,35 +5,37 @@
 const int screen_N=35;//size of the screen
 const int screen_M=100;
 
-//for initializing the game, e.g. new a bird
+//for initializing the setting game, e.g. new a bird 
 Bird* init_game(void);
 
-// print the screen
+//to print the screen
 void print_screen(void);
 
 //to move a bird；
 //input the user-defined type Bird
 void move_bird(Bird &B);
 
-//to move all the obstacles, which are store in STL_list, also delete the obstacles that are out
+//to move all the obstacles, which are store in STL_list
 void move_obstacles(void);
 
-//to check if the bird hit an obstacle; and check if the bird pass an obstacle
+//to check if the bird hit an obstacle; and check if the bird pass an obstacle; also delete the obstacles that are out
 //input bird type parameters and the current score
+//output a bool type, true for hitting the obstacle;
 bool check_fail(Bird B, int &score);
 
 //check if an obstacle will be added now
 //input t is the time (or the distance from the right-most edge of the screen) that the previous obsatcle appears
+//output a bool type, true for adding an obstacle
 bool check_new_ob(int t);
 
 //make an ob and push_back into the list
 void add_ob(void);
 
-//add the bird to screen
+//add the bird to the array screen
 //input the bird type parameters
 void add_bird_to_screen(Bird B);
 
-//add the obstacles to screen
+//add the obstacles to the array screen
 void add_ob_to_screen(void);
 
 //to start the game
@@ -43,10 +45,10 @@ int startgame(void);
 //input the bird type parameters
 void bird_fall(Bird B);
 
-//to initialize all the entries in screen to be ' '
+//to initialize all the entries in the array screen
 void init_screen(void);
 
-//to pause the game
+//to pause the game, return true if the user command to return to the main menu
 bool game_pause(void);
 
 #endif
