484eba7e00088f05af8e5ac962adf16eac5720fd
ezz118 <146748838+ezz118@users.noreply.github.com>
2023-11-29 15:50:11 +0800

    Update menu.h: modify the comment

--- menu.h
+++ menu.h
@@ -5,7 +5,7 @@
 using namespace std;
 
 // a function to update the ranking list;
-// input the score and the name of the player
+// input the score and of the player
 void upd_ranking(int score);
 
 // a function to display the ranking list
@@ -14,7 +14,7 @@ void showranking(void);
 // a function to print out the menu page
 void print_menu(void);
 
-// a function to pause and enter the next phase
+// a function to pause and wait for further command for user
 void wait_to_enter(void);
 
 // a function to end the game
